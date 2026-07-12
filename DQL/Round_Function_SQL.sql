3b95401f4b545081e68ff53aea89fa093cdff818
Monish Mansuri <monish151mansuri@gmail.com>
2026-07-12 12:51:15 +0530

    add_round_functions
    deep understanding round functions in sql

---
+++ DQL/Round_Function_SQL.sql
@@ -0,0 +1,16 @@
+
+select 254.2, round( 542.4,-3);
+select 254.2, round( 542.4,-2);
+select 254.2, round( 542.4,-1);
+select 254.2, round( 542.4,-1);
+select 254.2, round( 542.45,2);
+select 254.2, round( 542.4,-1);
+select 254.2, round( 542.4,-2);
+select 254.2, round( 542.4,-1);
+select 254.2, round( 542.4,-3);
+select 254.2, round( 542.45,2);
+select 254.2, round( 542.4,-1);
+select 254.2, round( 542.3,1);
+select 254.2, round( 542.4,1);
+select 254.2, round( 542.423,2);
+select 254.2, round( 542.422,3);
