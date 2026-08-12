52f1ac04dd721318e6854c8b4ad30caac3f230d4
Monish Mansuri <monish151mansuri@gmail.com>
2026-08-12 16:59:25 +0530

    pracitce_questions_of_windows_Funtions

---
+++ questions_windows_functions.sql
@@ -0,0 +1,95 @@
+show databases;
+create database windowsdb;
+use windowsdb;
+
+CREATE TABLE orders (
+    order_id INT,
+    order_date DATE,
+    customer_id INT,
+    customer_name VARCHAR(50),
+    city VARCHAR(30),
+    category VARCHAR(30),
+    product VARCHAR(50),
+    quantity INT,
+    amount DECIMAL(10,2)
+);
+
+
+INSERT INTO orders
+(order_id, order_date, customer_id, customer_name, city, category, product, quantity, amount)
+VALUES
+(1001, '2026-01-05', 201, 'Amit',   'Jaipur',  'Electronics', 'Laptop',       1, 65000),
+(1002, '2026-01-10', 202, 'Neha',   'Delhi',   'Electronics', 'Mobile',       2, 50000),
+(1003, '2026-01-15', 201, 'Amit',   'Jaipur',  'Furniture',   'Chair',        4, 12000),
+(1004, '2026-01-20', 203, 'Rahul',  'Mumbai',  'Electronics', 'Headphones',   3, 9000),
+(1005, '2026-01-25', 204, 'Priya',  'Delhi',   'Clothing',    'Jacket',       2, 8000),
+
+(1006, '2026-02-03', 201, 'Amit',   'Jaipur',  'Electronics', 'Mobile',       1, 28000),
+(1007, '2026-02-08', 202, 'Neha',   'Delhi',   'Furniture',   'Table',        1, 15000),
+(1008, '2026-02-12', 203, 'Rahul',  'Mumbai',  'Clothing',    'Shoes',        2, 10000),
+(1009, '2026-02-18', 205, 'Vikas',  'Pune',    'Electronics', 'Laptop',       1, 70000),
+(1010, '2026-02-25', 204, 'Priya',  'Delhi',   'Furniture',   'Sofa',         1, 35000),
+
+(1011, '2026-03-02', 201, 'Amit',   'Jaipur',  'Clothing',    'Shirt',        5, 7500),
+(1012, '2026-03-07', 202, 'Neha',   'Delhi',   'Electronics', 'Headphones',   2, 6000),
+(1013, '2026-03-11', 203, 'Rahul',  'Mumbai',  'Electronics', 'Laptop',       1, 68000),
+(1014, '2026-03-16', 205, 'Vikas',  'Pune',    'Furniture',   'Chair',        6, 18000),
+(1015, '2026-03-21', 204, 'Priya',  'Delhi',   'Clothing',    'Shoes',        1, 5500),
+
+(1016, '2026-04-04', 201, 'Amit',   'Jaipur',  'Electronics', 'Tablet',       1, 22000),
+(1017, '2026-04-09', 202, 'Neha',   'Delhi',   'Clothing',    'Jacket',       1, 4500),
+(1018, '2026-04-14', 203, 'Rahul',  'Mumbai',  'Furniture',   'Table',        2, 30000),
+(1019, '2026-04-20', 205, 'Vikas',  'Pune',    'Electronics', 'Mobile',       2, 52000),
+(1020, '2026-04-27', 204, 'Priya',  'Delhi',   'Electronics', 'Laptop',       1, 72000);
+-- assigment work class
+select * from orders;
+select customer_id,customer_name, order_date,row_number() over(partition by customer_id order by order_date) from orders;
+-- Q2 
+select customer_id,customer_name, amount,row_number() 
+over( order by amount desc) from orders;
+
+select order_id, category ,amount,rank() 
+over( partition by category order by amount desc) as Ranking_by_amount from orders;
+-- Q3
+select customer_id,customer_name, amount,row_number() 
+over( order by amount desc) from orders;
+
+-- Q3 find the differnce between order amount and avg() amount of its category 
+
+select order_id,category,amount,avg(amount)  over(partition by category) ,
+round( amount - avg(amount)  over(partition by category),2) from orders;
+
+-- Q4 find the highest maximum order every customers 
+select * from  
+(select customer_id,customer_name,order_id, amount,
+max(amount) over(partition by customer_id) as max_amount from orders )as t
+where amount = max_amount; 
+
+-- # same question with rank functions Q4 and Q5 is same 
+select * from  
+(select customer_id,customer_name,order_id, amount,
+rank () over(partition by customer_id order by amount desc)c_rank from orders )as t
+where  c_rank =1; 
+
+-- Q6 find the  top two highest value of every customers;
+select * from  
+(select customer_id,customer_name,order_id, amount,
+rank () over(partition by customer_id order by amount desc)c_rank from orders )as t
+where  c_rank <=2;
+
+
+-- Q7 find the secound highest order every customers 
+select * from  
+(select customer_id,customer_name,order_id, amount,
+rank () over(partition by customer_id order by amount desc)c_rank from orders )as t
+where  c_rank =2; 
+
+-- Q8 calculate the percentage contribution each order to customers total spending 
+SELECT 
+    *,
+    SUM(amount) OVER(PARTITION BY customer_id) AS total_spending,
+    (amount * 100.0) / SUM(amount) OVER(PARTITION BY customer_id) AS percentage_contribution
+FROM orders;
+
+
+
